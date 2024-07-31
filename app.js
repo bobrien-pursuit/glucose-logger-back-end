c46da69a7345d0b1379b040ddc40ebd6f008886a
Brendan O'Brien <bobrien@pursuit.org>
2024-07-31 14:34:25 -0400

    creates app.js creates entriescontroller

--- app.js
+++ app.js
@@ -11,5 +11,15 @@ app.use(express.json());
 
 //ROUTES
 
+//localhost:4001
+app.get('/', (req, res) => {
+    res.send("Glucose Logger")
+});
+
+// 404 page
+app.get('*', (req, res) => {
+    res.status(404).send("Page not found");
+})
+
 //EXPORTS
 module.exports = app;
